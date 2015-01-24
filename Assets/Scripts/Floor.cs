d9202b6b1512a4a7187d36d610bcd5c4eebd5cea
iuval <iuvalgoldansky@gmail.com>
2015-01-24 03:37:18 -0200

    Add hidden spikes and wall to floor.

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Floor : MonoBehaviour {
 
+	GameObject spikes;
+	GameObject wall;
+
 	// Use this for initialization
 	void Start () {
-	
+		foreach (Transform t in transform) {
+			if (t.name == "Spikes") spikes = t.gameObject;
+			if (t.name == "Wall") wall = t.gameObject;
+		}
+		spikes.SetActive (false);
+//		wall.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
 	}
 
 	public bool IsOutside() {
